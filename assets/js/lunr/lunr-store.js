4a8b19b89512eba9c79154f408185390c3ec7a6c
purushoth-kumar <purushoth-kumar@users.noreply.github.com>
2025-07-25 15:08:37 +0000

    deploy: 0de49effb230e3bee678b426c8477246ae3d9dc9

---
+++ assets/js/lunr/lunr-store.js
@@ -0,0 +1,13 @@
+var store = [{
+        "title": "Welcome to Jekyll!",
+        "excerpt":"You’ll find this post in your _posts directory. Go ahead and edit it and re-build the site to see your changes. You can rebuild the site in many different ways, but the most common way is to run jekyll serve, which launches a web server and auto-regenerates your site when...","categories": ["jekyll","update"],
+        "tags": [],
+        "url": "/jekyll/update/2025/07/25/welcome-to-jekyll.html",
+        "teaser": null
+      },{
+        "title": "Entry Fee Recommendation System",
+        "excerpt":"Overview Built a Contextual Multi-Armed Bandit using XGBoost and SquareCB to improve user retention and revenue for Games 24x7. Used IPW to de-bias training data Observed 7% MoM retention and 1.2% ARPU lift during experimentation Skills &amp; Tools Python, XGBoost, Bandit Algorithms, Data Science Impact Improved user retention and revenue...","categories": [],
+        "tags": [],
+        "url": "/projects/entry-fee-recommendation/",
+        "teaser": null
+      }]
